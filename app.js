809d8fc4d8e6a6fd3174451b3e3576322cf30c6b
vishalmane11 <85506357+vishalmane11@users.noreply.github.com>
2024-02-03 17:48:55 +0530

    origijn change

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(
 app.use(
   cors({
     credentials: true,
-    origin: "*",
+    origin: "https://mcaburgerwala-one.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
