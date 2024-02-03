f14f282319919c092467b70dc76a99d736d88f67
vishalmane11 <85506357+vishalmane11@users.noreply.github.com>
2024-02-03 17:37:35 +0530

    add env

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,7 @@ router.get(
 router.get(
   "/login",
   passport.authenticate("google", {
-    successRedirect: "http://localhost:3000/",
+    successRedirect: process.env.FRONTEND_URL,
   })
 );
 
